0176f33634a6ad97e63146477a16d3c575946a0b
Joe Hancock <102306663+25713655@users.noreply.github.com>
2022-12-12 13:00:11 +0000

    2nd Commit

--- day/Assets/Scripts/DayCycleController.cs
+++ day/Assets/Scripts/DayCycleController.cs
@@ -6,13 +6,38 @@ using UnityEngine.UI;
 
 public class DayCycleController : MonoBehaviour
 {
-    public Button houseButton;
-    public Button treeButton;
+    // ATK, DEF, CHR, SPD
+    int[,] partyStats = { { 1, 1, 1, 1 }, { 1, 1, 1, 1 }, { 1, 1, 1, 1 } };
+
+    //Sprites
+    public Sprite houseMeetupBackgroundSprite1, houseMeetupBackgroundSprite2, houseMeetupBackgroundSprite3, parkMeetupBackgroundSprite;
+
+    //Backgrounds
+    Sprite meetupBackgroundSprite, statsChangeBackgroundSprite;
+    Animator meetupBackgroundAnimator, statsChangeBackgroundAnimator;
+
+    //Buttons
+    GameObject houseButton, parkButton;
+
+    //houseButton.GetComponentInChildren<TextMeshProUGUI>().text = "Now works.";
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject meetupBackground = GameObject.Find("MeetupBackground");
+        GameObject statsChangeBackground = GameObject.Find("StatsChangeBackground");
+
+        //Sprites
+        meetupBackgroundSprite = meetupBackground.GetComponent<Image>().sprite;
+        statsChangeBackgroundSprite = statsChangeBackground.GetComponent<Image>().sprite;
+
+        //Animators
+        meetupBackgroundAnimator = meetupBackground.GetComponent<Animator>();
+        statsChangeBackgroundAnimator = statsChangeBackground.GetComponent<Animator>();
+
+        //Buttons
+        houseButton = GameObject.Find("HouseButton"); //Needed? Or just get specific component?
+        parkButton = GameObject.Find("ParkButton");
     }
 
     // Update is called once per frame
@@ -21,25 +46,35 @@ public class DayCycleController : MonoBehaviour
         
     }
 
+    // **********
+    //  Buttons
+    // **********
+
+    //Locations
     public void HouseOnClick()
     {
-        GameObject one = GameObject.Find("HouseButton");
-        Debug.Log(one);
+        meetupBackgroundSprite = houseMeetupBackgroundSprite1;
+        meetupBackgroundAnimator.SetTrigger("MoveIn");
+    }
+    public void ParkOnClick()
+    {
 
-        for (int i = 0; i < one.GetComponents<Component>().Length; i++)
-        {
-
-        }
-
-        TextMesh two = one.GetComponentInChildren<TextMesh>();
-        Debug.Log(two);
-        string three = two.text;
-        Debug.Log(three);
-        //GameObject.FindGameObjectWithTag("Button").GetComponentInChildren<Text>().text = "Still doesn't work.";
-        //houseButton.GetComponentInChildren<Text>().text = "Doesn't work";
     }
 
-    public void TreeOnClick()
+    //Dialogue
+    public void DialogueChoice1OnClick()
+    {
+        meetupBackgroundSprite = houseMeetupBackgroundSprite2;
+        statsChangeBackgroundAnimator.SetTrigger("MoveIn");
+    }
+    public void DialogueChoice2OnClick()
+    {
+        meetupBackgroundSprite = houseMeetupBackgroundSprite3;
+        statsChangeBackgroundAnimator.SetTrigger("MoveIn");
+    }
+
+    //Stats Change
+    public void StatsChangeConfirmationOnClick()
     {
 
     }
